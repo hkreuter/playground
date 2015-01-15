b447f631b8ad564984732dbce20ee14b22545596
hkreuter <heike@reuter-consulting.org>
2015-01-15 21:54:21 +0100

    added project's vm configuration

---
+++ vmconfig/environments/start.pp
@@ -0,0 +1,14 @@
+node thebox {
+
+    include apt-get-update
+    include apt-get-install
+    include sources
+    include php5
+    include apache2
+    include curl
+    include mysql
+    include phpmyadmin
+    include composer
+    include symfony
+
+}
