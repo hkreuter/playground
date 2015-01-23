71824c980addd74eb2ce11427ff457f99cd15869
hkreuter <heike@reuter-consulting.org>
2015-01-24 00:08:02 +0100

    wip

---
+++ src/HrBundle/Controller/GamesController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: H.R.
+ * Date: 15/01/15
+ * Time: 23:10
+ */
+
+namespace HrBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GamesController extends Controller
+{
+    private $_games = array( 'simulation', 'solitaire', 'gems', 'memory', 'tiles' );
+
+    /**
+     * @Route("/app/games/{page}", name="start"), defaults={"page" = 'index'})
+     */
+    public function indexAction($page)
+    {
+        if ( !in_array($page, $this->_games) ) {
+            $page = 'index';
+        }
+
+        $template = "games/$page.html.twig";
+
+        return $this->render($template);
+    }
+
+}
